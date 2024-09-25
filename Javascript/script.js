2fd5519a43072aad755f8204ab3a253d4bfd3ef6
Arman Muharrir | Cod <arsyoki9206@gmail.com>
2024-09-26 03:25:01 +0700

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,4 @@
-//Icon Navbar
+//?Icon Navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -6,7 +6,8 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
-// scroll section
+
+//! scroll section
 let section = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
@@ -26,11 +27,11 @@ window.onscroll = () => {
 
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
-    menuIcon.classList.remove('bx-x');
+    menuIcon.classList.remove('bx-x');  
     navbar.classList.remove('active');
-}
+};
 
-// Input Email
+//! Input&output Email
 const form = document.querySelector("form");
 const fullName = document.getElementById("name");
 const email = document.getElementById("email");
@@ -39,30 +40,35 @@ const subject = document.getElementById("subject");
 const mess = document.getElementById("message");
 
 function sendEmail() {
+    const bodyMessage = `
+        Name: ${fullName.value} <br/>
+        Email: ${email.value} <br/>
+        Phone: ${phone.value} <br/>
+        Message: ${mess.value}`;
 
+//! Smtpjs.com
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "armanulf009@gmail.com",
-        Password : "C0F34015800AA4796E9D01A1A900814F2D19",
-        To : 'armanulf009@gmail.com',
-        From : "armanulf009@gmail.com",
-        Subject : subject.value,
-        Body : bodyMessage
+        Host: "smtp.elasticemail.com",
+        Username: "armanulf009@gmail.com",
+        Password: "C0F34015800AA4796E9D01A1A900814F2D19",
+        To: 'armanulf009@gmail.com',
+        From: "armanulf009@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
     }).then(
-            message => {
-                if (message == "OKE") {
-                    Swal.fire({
-                        title: "Good job!",
-                        text: "You clicked the button!",
-                        icon: "success"
-                    });
-                }
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Terima Kasih!",
+                    text: "Email Terkirim!",
+                    icon: "Succes"
+                });
             }
+        }
     );
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
     sendEmail();
 });
